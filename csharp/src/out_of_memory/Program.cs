3472416c160dc07023da3a147f68ae869655987a
Unknown <vpyadukhov@outlook.com>
2019-01-27 16:07:05 +0300

    add csharp out_of_memory

---
+++ csharp/src/out_of_memory/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OutOfMemoryException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            throw new System.OutOfMemoryException();
+        }
+    }
+}
