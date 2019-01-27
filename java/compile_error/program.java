81ec605c51bfc9d09dfdc405fc594eff76987237
Unknown <vpyadukhov@outlook.com>
2019-01-27 14:40:53 +0300

    modified gitignore for java, add java projects

---
+++ java/compile_error/program.java
@@ -0,0 +1,65 @@
+import java.io.*;
+
+public class Test {
+    public static void main(String [] args) {
+
+        // The name of the file to open.
+        String fileName = "input.txt";
+
+        // This will reference one line at a time
+        String line = null;
+        String inputText = "";
+
+        try {
+            // FileReader reads text files in the default encoding.
+            FileReader fileReader = 
+                new FileReader(fileName);
+
+            // Always wrap FileReader in BufferedReader.
+            BufferedReader bufferedReader = 
+                new BufferedReader(fileReader);
+
+            while((line = bufferedReader.readLine()) != null) {
+                inputText += line;
+            }   
+
+            // Always close files.
+            bufferedReader.close();         
+        }
+        catch(FileNotFoundException ex) {
+            System.out.println(
+                "Unable to open file '" + 
+                fileName + "'");                
+        }
+        catch(IOException ex) {
+            System.out.println(
+                "Error reading file '" 
+                + fileName + "'");                  
+            // Or we could just do this: 
+            // ex.printStackTrace();
+        }
+
+        try {
+            // Assume default encoding.
+            FileWriter fileWriter =
+                new FileWriter("output.txt");
+
+            // Always wrap FileWriter in BufferedWriter.
+            BufferedWriter bufferedWriter =
+                new BufferedWriter(fileWriter);
+
+            // Note that write() does not automatically
+            // append a newline character.
+            bufferedWriter.write(inputText);
+            // Always close files.
+            bufferedWriter.close();
+        }
+        catch(IOException ex) {
+            System.out.println(
+                "Error writing to file '"
+                + fileName + "'");
+            // Or we could just do this:
+            // ex.printStackTrace();
+        }
+    }
+}
