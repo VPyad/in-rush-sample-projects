d7ea7ce6ad55ed2dd9adb98e8544ce032a999a14
Unknown <vpyadukhov@outlook.com>
2019-01-27 14:34:38 +0300

    add cpp projects

---
+++ cpp/out_of_memory/program.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+using std::cin;
+
+int main()
+{
+	unsigned long long i = 0;
+
+	while(true)
+	{
+		int* a = new int;
+		i++;
+	}
+
+	return 0;
+}
