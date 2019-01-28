0bca4adc42515b0ce664f78c01171e219dc9105d
Unknown <vpyadukhov@outlook.com>
2019-01-28 19:07:17 +0300

    add cpp runtime and compile errors project

---
+++ cpp/compile_error/program.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+using std::cin;
+
+int main()
+{
+	string line, text;
+
+	ifstream infile("input.txt")
+  if (infile.is_open())
+  {
+    while (getline (infile, line))
+    {
+      text += line;
+    }
+    infile.close();
+  }
+
+  else printf("Unable to open file"); 
+
+
+	ofstream myfile("output.txt");
+	if (myfile.is_open())
+	{
+		myfile << text;
+		myfile.close();
+	}
+	else printf("Unable to open file");
+
+	return 0;
+}
---
+++ cpp/runtime_error/program.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	std::string ().replace (100, 1, 1, 'c');
+	return 0;
+}
