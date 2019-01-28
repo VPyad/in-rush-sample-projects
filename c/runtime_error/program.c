866d11ae19be130ab93f577dc77ff3f63363f409
Unknown <vpyadukhov@outlook.com>
2019-01-28 20:44:52 +0300

    modified c projects, runtime error does not log error in shell
    in ubuntu however dous

--- c/runtime_error/program.c
+++ c/runtime_error/program.c
@@ -1,32 +1,10 @@
 #include <stdio.h>
-#define MAXCHAR 1000
-int main ()
+#include <string.h>
+#include <cstdlib>
+
+int main(void)
 {
+    *(int*)0 = 0;
 
-FILE *fp;
-    char str[MAXCHAR];
-    char text[MAXCHAR];
-    //char* filename = 'input.txt';
- 
-    fp = fopen("input.txt", "r");
-    /*if (fp == NULL){
-        printf("Could not open file"); // %s",filename);
-        return 1;
-    }*/
-    while (fgets(str, MAXCHAR, fp) != NULL)
-    {}    //text += str;
-    fclose(fp);
-
-FILE *f = fopen("output.txt", "w");
-
-if (f == NULL)
-{
-    printf("Error opening file!\n");
     return 0;
-}
-
-fprintf(f, "Some text: %s\n", str);
-
-fclose(f);
-return 0;
 }
